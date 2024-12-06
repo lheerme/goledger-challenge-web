e207d1bcc4f2e32d09e96d65af8cb79b304a9aca
Guilherme Souza <gsouza2651@gmail.com>
2024-12-06 16:10:12 -0300

    style: implement tailwind-merge and centered content

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,18 @@
 import { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
 
 export type ButtonProps = ComponentProps<'button'>
 
 export function Button(props: ButtonProps) {
+  const { className, ...rest } = props
+
   return (
     <button 
-      {...props}
-      className="bg-violet-400 text-zinc-950 flex items-center gap-1 px-3 py-2 rounded-lg hover:opacity-80 hover:transition-opacity"
+      {...rest}
+      className={twMerge(
+        'bg-violet-400 text-zinc-950 flex justify-center items-center gap-1 px-3 py-2 rounded-lg hover:opacity-80 hover:transition-opacity',
+        className
+      )}
     />
   )
 }
