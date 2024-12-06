882225c7db87fc6af3bbfc002fb92436c11976e7
Guilherme Souza <gsouza2651@gmail.com>
2024-12-06 20:21:18 -0300

    feat: added Toaster component

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient()
 
@@ -8,6 +9,7 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={routes} />
+      <Toaster />
     </QueryClientProvider>
   )
 }
