9fc8ce9fc1c381062eb8f12e50f0bf3f94c91fa3
Guilherme Souza <gsouza2651@gmail.com>
2024-12-09 10:22:15 -0300

    refactor: organize playlists as folders containing index.tsx and related files

--- src/pages/app/playlists/index.tsx
+++ src/pages/app/playlists/index.tsx
@@ -1,6 +1,6 @@
 export function Playlists() {
   return (
-    <div className="flex w-full h-full flex-col gap-8 p-6">
+    <div className="flex w-full min-h-full flex-col gap-8 p-6">
       <h1 className="text-3xl font-semibold">Playlists</h1>
     </div>
   )
